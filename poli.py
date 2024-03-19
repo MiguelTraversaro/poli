6dd9cd10f59036dc82ada2cb9c13f0d65543da68
MiguelTraversaro <70926143+MiguelTraversaro@users.noreply.github.com>
2024-03-18 21:55:21 -0300

    Update poli.py

--- poli.py
+++ poli.py
@@ -6,8 +6,8 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 
 # app config
-st.set_page_config(page_title="💬 POLI 🤖")
-st.title('💬 POLI Chatbot 🤖')
+st.set_page_config(page_title="💬 POLI 🤖",page_icon="🤖💬")
+st.title('💬 POLI Chatbot 🤖',centered="true")
 # app sidebar
 with st.sidebar:
     st.title('🤖💬 POLI Chatbot')
