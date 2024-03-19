1e1e30fe02f3d64d114b5cc718eae6045fb99aec
MiguelTraversaro <70926143+MiguelTraversaro@users.noreply.github.com>
2024-03-19 20:10:34 -0300

    Update poli.py

--- poli.py
+++ poli.py
@@ -105,7 +105,7 @@ def click(string):
     # botones_placeholder.empty()
     st.session_state.chat_history.append(HumanMessage(content=string))
     response = "prueba: "+st.write_stream(get_response(user_query, st.session_state.chat_history))
-    st.session_state.chat_history.append(AIMessage(content=response))
+    st.session_state.chat_history.append(AIMessage(content="prueba2"+response))
     st.session_state.boton_clickeado = True
 
 botones_placeholder = st.empty() 
