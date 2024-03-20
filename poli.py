dec5f1aac2bdbf9696e91199e91dbeacf9921d44
MiguelTraversaro <70926143+MiguelTraversaro@users.noreply.github.com>
2024-03-20 01:23:18 -0300

    Update poli.py

--- poli.py
+++ poli.py
@@ -96,7 +96,7 @@ if user_query is not None and user_query != "":
     with st.chat_message("👨‍💻"):
         st.markdown(user_query)
     with st.chat_message("📎"):
-        response = st.write(get_response(user_query, st.session_state.chat_history))
+        response = get_response(user_query, st.session_state.chat_history)
     st.session_state.chat_history.append(AIMessage(content=response))
 elif not st.session_state.boton_clickeado:
     botones_placeholder = st.empty()
