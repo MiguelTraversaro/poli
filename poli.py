eaa35846f1b88034ddb7b5b9bc5d9d4a474c6d0d
MiguelTraversaro <70926143+MiguelTraversaro@users.noreply.github.com>
2024-03-20 01:22:04 -0300

    Update poli.py

--- poli.py
+++ poli.py
@@ -85,8 +85,7 @@ for message in st.session_state.chat_history:
 
 def click(string):
     st.session_state.chat_history.append(HumanMessage(content=string))
-    response = st.write(get_response(string, st.session_state.chat_history))
-    st.write(response)
+    response = get_response(string, st.session_state.chat_history)
     st.session_state.chat_history.append(AIMessage(content=response))
     st.session_state.boton_clickeado = True
     botones_placeholder.empty()
