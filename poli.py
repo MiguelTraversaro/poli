bc19d6c2710cdf7e1d527456d3af342ed6d250a2
MiguelTraversaro <70926143+MiguelTraversaro@users.noreply.github.com>
2024-03-20 01:02:13 -0300

    Update poli.py

--- poli.py
+++ poli.py
@@ -89,7 +89,6 @@ def click(string):
     response = st.write_stream(get_response(string, st.session_state.chat_history))
     st.session_state.chat_history.append(AIMessage(content=response))
     st.session_state.boton_clickeado = True
-    st.rerun
 
 user_query = st.chat_input("Type your message here...")            
 if user_query is not None and user_query != "":
@@ -104,12 +103,12 @@ elif not st.session_state.boton_clickeado:
         with botones_placeholder.container():
             col1,col2 = st.columns(2)
             with col1:
-                if st.button(opciones_mensajes[0],use_container_width=True, on_click=click, args=[opciones_mensajes[0]]):
-                    botones_placeholder.empty()
-                if st.button(opciones_mensajes[2],use_container_width=True, on_click=click, args=[opciones_mensajes[2]]):
-                    botones_placeholder.empty()
+                if st.button(opciones_mensajes[0],use_container_width=True):
+                    click(opciones_mensajes[0])
+                if st.button(opciones_mensajes[2],use_container_width=True):
+                    click(opciones_mensajes[2])
             with col2:
-                if st.button(opciones_mensajes[1],use_container_width=True, on_click=click, args=[opciones_mensajes[1]]):
-                    botones_placeholder.empty()
-                if st.button(opciones_mensajes[3],use_container_width=True, on_click=click, args=[opciones_mensajes[3]]):
-                    botones_placeholder.empty()
+                if st.button(opciones_mensajes[1],use_container_width=True):
+                    click(opciones_mensajes[1])
+                if st.button(opciones_mensajes[3],use_container_width=True):
+                    click(opciones_mensajes[3])
