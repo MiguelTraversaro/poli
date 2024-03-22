c772bf1f228ffe29a60778de1dab3407c7af4416
MiguelTraversaro <70926143+MiguelTraversaro@users.noreply.github.com>
2024-03-22 15:08:29 -0300

    Update poli.py

--- poli.py
+++ poli.py
@@ -64,7 +64,7 @@ def get_response(user_question, chat_history):
     """
     prompt = ChatPromptTemplate.from_template(template)
 
-    llm = ChatOpenAI(openai_api_key=openai.api_key,model="gpt-3.5-turbo-0125",temperature=0.8,model_kwargs={"top_p":0.8})
+    llm = ChatOpenAI(openai_api_key=openai.api_key,model="gpt-3.5-turbo-0125",temperature=0.1,model_kwargs={"top_p":0.1})
         
     chain = prompt | llm | StrOutputParser()
     
